31a497b7f03e6b4f1b765f3c3030a246921e8f98
Магомедали Гаджиев <mag198421@gmail.com>
2025-06-20 14:28:39 +0300

    Add project files.

---
+++ BallGamesNewWinFormsApp/MainForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BallGamesNewWinFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            drawingButton = new Button();
+            SuspendLayout();
+            // 
+            // drawingButton
+            // 
+            drawingButton.Location = new Point(639, 56);
+            drawingButton.Name = "drawingButton";
+            drawingButton.Size = new Size(117, 23);
+            drawingButton.TabIndex = 0;
+            drawingButton.Text = "Рисовать шарик";
+            drawingButton.UseVisualStyleBackColor = true;
+            drawingButton.Click += drawingButton_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(drawingButton);
+            Name = "MainForm";
+            Text = "Мячики";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button drawingButton;
+    }
+}
---
+++ BallGamesNewWinFormsApp/MainForm.cs
@@ -0,0 +1,15 @@
+namespace BallGamesNewWinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void drawingButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
