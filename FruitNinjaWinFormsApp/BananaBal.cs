8a52de9122d6a579e51122f2831a913601413094
Magomedali Gajiev <mag198421@gmail.com>
2025-07-04 21:26:09 +0300

    Добавил класс BananaBal

---
+++ FruitNinjaWinFormsApp/BananaBal.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FruitNinjaWinFormsApp
+{
+    public class BananaBal : FruitBall
+    {
+        public BananaBal(Form form) : base(form)
+        {
+            _color = Color.Yellow;
+            _radius = _random.Next(25, 36);
+        }
+    }
+}
