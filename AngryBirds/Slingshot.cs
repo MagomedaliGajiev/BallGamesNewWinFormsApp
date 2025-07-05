6a5d28ff4c6557c5fb5f1efce9d3c28cbe26f151
Magomedali Gajiev <mag198421@gmail.com>
2025-07-05 18:02:09 +0300

    Добавил  класс Slingshot

---
+++ AngryBirds/Slingshot.cs
@@ -0,0 +1,32 @@
+﻿namespace AngryBirds
+{
+    public class Slingshot
+    {
+        private Point _possition;
+        private const int Width = 30;
+        private const int Heigght = 100;
+        private readonly Pen _pen = new Pen(Color.Brown, 5);
+
+        public Slingshot(int x, int y)
+        {
+            _possition = new Point(x, y);
+        }
+
+        private void Draw(Graphics graphics)
+        {
+            // Рисуем рогатку
+            graphics.DrawLine(_pen,
+                _possition.X, _possition.Y + Heigght,
+                _possition.X - Width, _possition.Y);
+
+            graphics.DrawLine(_pen,
+                _possition.X, _possition.Y + Heigght,
+                _possition.X + Width, _possition.Y);
+        }
+
+        public Point GetPoint()
+        {
+            return new Point(_possition.X, _possition.Y);
+        }
+    }
+}
