fa5f158ab7e56a872e8d4cf25f2bd72d5e9ffa13
Magomedali Gajiev <mag198421@gmail.com>
2025-07-05 17:34:00 +0300

    Добавил класс PigBall

---
+++ AngryBirds/PigBall.cs
@@ -0,0 +1,15 @@
+﻿using Balls.Common;
+
+namespace AngryBirds
+{
+    public class PigBall : Ball
+    {
+        public PigBall(Form form, int centerX, int centerY) : base(form)
+        {
+            _centerX = centerX;
+            _centerY = centerY;
+            _radius = 15;
+            _color = Color.Green;
+        }
+    }
+}
